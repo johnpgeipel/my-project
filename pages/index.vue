a76fd7837185258a6fbacdd27a06e8dade85f35b
johnpgeipel <johnpgeipel@gmail.com>
2020-09-04 15:50:03 -0500

    added rel='...' to buttons

--- pages/index.vue
+++ pages/index.vue
@@ -12,10 +12,12 @@
         <a
           href="https://nuxtjs.org/"
           target="_blank"
+          rel="noopener"
           class="button--green">Documentation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
+          rel="noopener"
           class="button--grey">GitHub</a>
       </div>
     </div>
