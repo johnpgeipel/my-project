bbcf702361ee5e659c871946745a724fccd0dc89
johnpgeipel <johnpgeipel@gmail.com>
2020-09-10 14:51:28 -0500

    Styles for navBar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,20 +1,20 @@
 <template>
     <b-navbar toggleable="lg" type="light" variant="light" fixed="top">
         <b-navbar-brand>
-            <nuxt-link to="/">JPG</nuxt-link>
+            <nuxt-link class="navbarBrand" to="/">JPG</nuxt-link>
         </b-navbar-brand>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav >
+        <b-collapse id="nav-collapse" class="navCollapse" is-nav>
+            <b-navbar-nav class="navbarNav">
                 <b-nav-item>
-                    <nuxt-link to="/about">About</nuxt-link>
+                    <nuxt-link class="navbarLink" to="/about">About</nuxt-link>
                 </b-nav-item>
                 <b-nav-item>
-                    <nuxt-link to="/works">Works</nuxt-link>
+                    <nuxt-link class="navbarLink" to="/works">Works</nuxt-link>
                 </b-nav-item>
                 <b-nav-item>
-                    <nuxt-link to="/contact">Contact</nuxt-link>
+                    <nuxt-link class="navbarLink" to="/contact">Contact</nuxt-link>
                 </b-nav-item>
             </b-navbar-nav>
         </b-collapse>
@@ -25,10 +25,12 @@ export default {
     
 }
 </script>
-<style lang="css">
+<style lang="css" scoped>
 
-    nuxt-link {
+    .navbarBrand,
+    .navbarLink {
         color: black;
     }
+
     
 </style>
